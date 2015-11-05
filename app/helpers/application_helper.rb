77fdaeeecbfed1018ce7805d49bdaaec354cb1fd
thanh <truongthanh.bk56@gmail.com>
2015-11-05 15:36:04 +0700

    add logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 	def full_title(page_title = '' )
-		base_title = "ruby on rails tutorial sample app"
+		base_title = "Ruby on Rails Tutorial Sample App"
 		if page_title.empty?
 			base_title
 		else
-			page_title + "|" + base_title
+			page_title + " | " + base_title
 		end
 	end
 end
